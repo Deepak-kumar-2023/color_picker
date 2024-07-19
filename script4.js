6be22dce906cf440f7353d52af13903adf34af09
Deepak <kumardeeepak392@gmail.com>
2024-07-19 18:46:29 +0530

    color-picker

---
+++ script4.js
@@ -0,0 +1,25 @@
+
+// const coding=["java","rubi","jvs","python","golang"];
+
+// const value=coding.forEach((item)=>{
+//     // console.log(item);
+//     return item;
+
+// })
+
+// const myNums=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+// const nu=myNums.filter( (nums)  =>  nums>4)
+// // console.log(nu)
+// const nun=myNums.filter( (nums)  =>  {
+//     return nums>4})
+
+// let arr=[];
+//     myNums.forEach((nums)=>{
+// if(nums>4){
+//     arr.push(nums);
+// }
+//     })
+//     console.log(arr);
+
+
